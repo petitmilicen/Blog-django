ad226c8ad32389718a9b5fbaef73ac8aae1483ff
Dormir0 <127413966+petitmilicen@users.noreply.github.com>
2023-05-11 17:04:11 -0400

    links y templates

--- blog/core/urls.py
+++ blog/core/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('publicaciones/', views.posts, name='publicaciones'),
-    path('donacion/', views.donacion, name='donacion')
+    path('donacion/', views.donacion, name='donacion'),
+    path('logearse/', views.logearse, name='logearse'),
+    path('registrarse/', views.registrarse, name='registrarse'),
+    path('nueva-publicacion/', views.nueva_publicacion, name='nueva-publicacion')
 
 ]
--- blog/core/views.py
+++ blog/core/views.py
@@ -4,7 +4,16 @@ def index(request):
     return render(request, 'index.html')
 
 def posts(request):
-    return render(request, 'posts.html')
+    return render(request, 'publicaciones.html')
 
 def donacion(request):
     return render(request, 'donacion.html')
+
+def nueva_publicacion(request):
+    return render(request, 'nueva-publicacion.html')
+
+def registrarse(request):
+    return render(request, 'registrarse.html')
+
+def logearse(request):
+    return render(request, 'logearse.html')
