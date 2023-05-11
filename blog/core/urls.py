57451b2120c1ae6404bd72aebb7c98b9b4e45c56
petitmilicen <127413966+petitmilicen@users.noreply.github.com>
2023-05-11 12:02:58 -0400

    avance

--- blog/core/urls.py
+++ blog/core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('', views.index, name='index'),
+    path('publicaciones/', views.posts, name='publicaciones'),
+    path('donacion/', views.donacion, name='donacion')
+
 ]
--- blog/core/views.py
+++ blog/core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def posts(request):
+    return render(request, 'posts.html')
+
+def donacion(request):
+    return render(request, 'donacion.html')
