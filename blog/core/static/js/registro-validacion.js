2e4f0a0af627461269fd4e847b3c601e8c38a289
Rodrigo Freire Galindo <65997613+RorroS@users.noreply.github.com>
2023-05-17 21:35:47 -0400

    Nueva Pag y validacion registro

--- blog/core/static/js/registro-validacion.js
+++ blog/core/static/js/registro-validacion.js
@@ -1,43 +1,65 @@
 $(document).ready(function() {
-    $('#form-registro').validate({
-      rules: {
-        nombre: {
-          required: true,
-          minlength: 3
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        password: {
-          required: true,
-          minlength: 8
-        },
-        password_2: {
-          required: true,
-          minlength: 8,
-          equalTo: "#password"
-        }
+  $('#form-registro').validate({
+    rules: {
+      nombre: {
+        required: true,
+        minlength: 3
       },
-      messages: {
-        nombre: {
-          required: "Por favor, ingrese su nombre",
-          minlength: "El nombre tiene contener minimo 3 caracteres"
-        },
-        email: {
-          required: "Por favor, ingrese su correo electrónico",
-          email: "Por favor, ingrese un correo electrónico válido"
-        },
-        password: {
-          required: "Por favor, ingrese una contraseña",
-          minlength: "La contraseña debe tener al menos 8 caracteres"
-        },
-        password_2: {
-          required: "Por favor, confirme su contraseña",
-          minlength: "La contraseña debe tener al menos 8 caracteres",
-          equalTo: "Las contraseñas no coinciden"
-        }
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 8,
+        strongPassword: true
+      },
+      password_2: {
+        required: true,
+        minlength: 8,
+        equalTo: "#password"
       }
-    });
+    },
+    messages: {
+      nombre: {
+        required: "Por favor, ingrese su nombre",
+        minlength: "El nombre debe contener al menos 3 caracteres"
+      },
+      email: {
+        required: "Por favor, ingrese su correo electrónico",
+        email: "Por favor, ingrese un correo electrónico válido"
+      },
+      password: {
+        required: "Por favor, ingrese una contraseña",
+        minlength: "La contraseña debe tener al menos 8 caracteres",
+        strongPassword: "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial"
+      },
+      password_2: {
+        required: "Por favor, confirme su contraseña",
+        minlength: "La contraseña debe tener al menos 8 caracteres",
+        equalTo: "Las contraseñas no coinciden"
+      }
+    }
   });
-  
+
+  $.validator.addMethod("strongPassword", function(value, element) {
+    return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+  }, "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial");
+
+  $.validator.addMethod("validEmail", function(value, element) {
+    return this.optional(element) || /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+  }, "Por favor, ingrese un correo electrónico válido");
+
+  $.validator.addMethod("noSpace", function(value, element) {
+    return value.trim() !== "";
+  }, "El campo no puede contener solo espacios en blanco");
+
+  $.validator.addMethod("noSpecialCharacters", function(value, element) {
+    return this.optional(element) || /^[a-zA-Z0-9]+$/.test(value);
+  }, "El campo no puede contener caracteres especiales");
+
+  $.validator.addMethod("noCommonPasswords", function(value, element) {
+    var commonPasswords = ["123456", "password", "qwerty"]; // Agrega más contraseñas comunes aquí
+    return !commonPasswords.includes(value.toLowerCase());
+  }, "La contraseña no puede ser una contraseña común");
+});
