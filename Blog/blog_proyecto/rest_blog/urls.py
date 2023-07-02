1ec1a0bbe7b7c04de9be823aa4d8d47fd9e68c4a
Danilo <127413966+petitmilicen@users.noreply.github.com>
2023-07-02 13:34:33 -0400

    DELETE, PUT, GET por id

--- Blog/blog_proyecto/rest_blog/urls.py
+++ Blog/blog_proyecto/rest_blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from rest_blog.views import *
 
 urlpatterns = [
-    path('lista_publicaciones', lista_publicaciones, name='lista_publicaciones'),
-    path('lista_comentarios', lista_comentarios, name='lista_comentarios')
+    path('lista-publicaciones', lista_publicaciones, name='lista_publicaciones'),
+    path('lista-comentarios', lista_comentarios, name='lista_comentarios'),
+    
+    path('detalle-publicacion/<id>', detalle_publicacion, name="detalle_publicacion"),
+    path('detalle-comentario/<id>', detalle_comentario, name="detalle_comentario")
 ]
--- Blog/blog_proyecto/rest_blog/views.py
+++ Blog/blog_proyecto/rest_blog/views.py
@@ -31,3 +31,47 @@ def lista_comentarios(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_publicacion(request, id):
+    try:
+        publicacion = Publicacion.objects.get(id=id)
+        
+    except Publicacion.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PublicacionSerializer(publicacion)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PublicacionSerializer(publicacion, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        publicacion.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_comentario(request, id):
+    try:
+        comentario = Comentario.objects.get(id=id)
+        
+    except Comentario.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ComentarioSerializer(comentario)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ComentarioSerializer(comentario, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        comentario.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
