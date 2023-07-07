5afbeb1764bccabb7add4faef50b8c4c5a4268dd
Danilo <127413966+petitmilicen@users.noreply.github.com>
2023-07-07 19:30:43 -0400

    aa

--- Blog/blog_proyecto/core/views.py
+++ Blog/blog_proyecto/core/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView 
 from django.contrib.auth.models import User
 from .forms import EditarPerfilForm
 from django.urls import reverse_lazy
